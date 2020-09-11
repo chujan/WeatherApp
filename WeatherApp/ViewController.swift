28966f4d1beb0a2825c70e9105d7d46c7ba9e802
Chukwuemeka Jennifer <jennichuks9@gmail.com]>
2020-09-11 08:27:42 +0100

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Chukwuemeka Jennifer on 11/09/2020.
+//  Copyright © 2020 Chukwuemeka Jennifer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
